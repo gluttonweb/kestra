166881f73feb0849fa5ee0d05e55c2eb275c3438
Loïc Mathieu <loikeseke@gmail.com>
2026-05-18 17:27:01 +0200

    chore(system): combine queue indices and use queuName
    - Combine two indices in one, this was done before but forgotten in the new migration mecanism
    - Use queuename instead of a CRC32 as all other queues use that to create topics/queue/channels

---
+++ jdbc-mysql/src/main/resources/migrations/2.0.1-queue-index-and-type-mysql.sql
@@ -0,0 +1,6 @@
+DROP INDEX `ix_type__offset` ON queues;
+DROP INDEX `ix_created` ON queues;
+
+CREATE INDEX ix_created__type ON queues (`created`, `type`);
+
+ALTER TABLE queues ALTER COLUMN `type` TYPE VARCHAR(250);
