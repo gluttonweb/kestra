ce03449642f77709f5dc63e32c4986566e948a89
Steven Meek <42916669+ammeek@users.noreply.github.com>
2026-05-18 09:24:32 +0100

    fix: add NEXT_EXECUTION_DATE to fieldsWithSpecificConditions

--- jdbc/src/test/java/io/kestra/jdbc/repository/AbstractJdbcRepositoryTest.java
+++ jdbc/src/test/java/io/kestra/jdbc/repository/AbstractJdbcRepositoryTest.java
@@ -33,6 +33,7 @@ class AbstractJdbcRepositoryTest extends AbstractJdbcRepository {
         QueryFilter.Field.TAGS,
         QueryFilter.Field.LOCKED,
         QueryFilter.Field.LAST_TRIGGERED_DATE,
+        QueryFilter.Field.NEXT_EXECUTION_DATE,
         QueryFilter.Field.TIME_RANGE
     );
 
